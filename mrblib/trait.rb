ccded964dd9cac3d210333780ca45d9d503f6d2e
Terence Lee <hone02@gmail.com>
2015-07-13 11:27:33 -0400

    print out trait key and only display item if it exists

--- mrblib/trait.rb
+++ mrblib/trait.rb
@@ -2,13 +2,18 @@ Trait = Struct.new(:name, :researched, :item, :craft, :type, :piece)
 
 class Trait
   def to_s
-    <<STRING
+    string = <<STRING
 craft: #{craft}
 type: #{type}
 piece: #{piece["name"]} (#{piece["description"]})
-#{name}
+name: #{name}
 researched: #{researched}
-item: #{item}
 STRING
+
+    if item
+      "#{string}item: #{item}\n"
+    else
+      string
+    end
   end
 end
